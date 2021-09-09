cacba2c5d5349ac89e8646d88eb5bedf1fd1b2e3
Hagar M. Abd-Elzaher <hagarr.abdelzaher@gmail.com>
2021-09-09 12:14:13 +0200

    main feature done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import "package:audioplayers/audio_cache.dart";
 
 void main() => runApp(XylophoneApp());
 
@@ -8,10 +9,34 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            buildKey(1, Colors.red),
+            buildKey(2, Colors.orange),
+            buildKey(3, Colors.yellow),
+            buildKey(4, Colors.green),
+            buildKey(5, Colors.blue),
+            buildKey(6, Colors.purple),
+            buildKey(7, Colors.deepPurple),
+          ]),
         ),
       ),
     );
+  }
+
+  Expanded buildKey(int noteNumber, Color buttonColor) {
+    return Expanded(
+        child: FlatButton(
+            color: buttonColor,
+            onPressed: () {
+              playSound(noteNumber);
+            }));
+  }
+
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play("assets_note$noteNumber.wav");
   }
 }
